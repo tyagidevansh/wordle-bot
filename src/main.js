5aefb3d2ce67bc2999b2501766169796d0834604
tyagidevansh <devanshtyagi0607@gmail.com>
2025-10-19 00:44:18 +0530

    wip

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const ANSWER = "CARDS";
+const ANSWER = "CAADS";
 let wordList = null;
 let focusedIdx = 0;
 
@@ -6,6 +6,10 @@ let inputs = null;
 let keys = null;
 
 let canMakeGuesses = true;
+
+let greenChar = ['', '', '', '', ''];
+let yellowChar = [];
+let grayChar = [];
 
 document.addEventListener("DOMContentLoaded", () => {
   fetch("valid-wordle-words.txt")
@@ -18,6 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
     })
     .then((data) => {
       wordList = [data.split("\n")];
+      wordList = wordList[0];
     });
 
   inputs = document.querySelectorAll('input[maxlength="1"]');
@@ -89,7 +94,7 @@ const handleOnScreenKey = (key) => {
   }
 };
 
-const handleBackspaceAndEnter = (event = null, key=null) => {
+const handleBackspaceAndEnter = (event = null, key = null) => {
   let curIdx = focusedIdx;
 
   if (event.key == "Backspace" || key == "Backspace") {
@@ -135,6 +140,11 @@ const checkGuess = (form, guess) => {
     if (guessChar == answerChar) {
       correct++;
       colors[i] = "green";
+      greenChar[i] = answer[i];
+      if (yellowChar.includes(answer[i])) {
+        let idx = yellowChar.indexOf(answer[i]);
+        yellowChar.splice(idx, 1);
+      }
       answer[i] = "x";
 
       // let idx = answer.charCodeAt(i);
@@ -150,25 +160,137 @@ const checkGuess = (form, guess) => {
     if (colors[i] == "green") continue;
 
     let guessChar = guess[i];
-
+    let tempYellowChar = [];
     for (j = 0; j < 5; j++) {
       if (i == j) continue;
       let answerChar = answer[j];
 
       if (guessChar === answerChar) {
         colors[i] = "yellow";
+        tempYellowChar.push(guess[i]);
         answer[j] = "x"; // this character (lowercase) will never occur in the answer so im using it to ensure no character is counted twice
       }
     }
+    for (idx = 0; idx < tempYellowChar.length; idx++) {
+      let curChar = tempYellowChar[idx];
+      if (!yellowChar.includes(curChar)) {
+        yellowChar.push(curChar);
+        break;
+      }
+
+      let numOccurences = 1;
+      for (idx2 = idx + 1; idx2 < tempYellowChar.length; idx2++) {
+        if (curChar === tempYellowChar[idx2]) {
+          numOccurences++;
+          tempYellowChar[idx2] = 'x';
+        }
+      }
+
+      for (idx3 = 0; idx3 < grayChar.length; idx3++) {
+        if (greenChar[idx3] === curChar) numOccurences--;
+      }
+
+      for (idx3 = 0; idx3 < yellowChar.length; idx3++) {
+        if (yellowChar[idx3] === curChar) numOccurences--;
+      }
+
+      for (idx4 = numOccurences; idx4 > 0; i++) {
+        yellowChar.push(curChar);
+      }
+    }
+    if (colors[i] == "gray" && !grayChar.includes(guess[i])) {
+      grayChar.push(guess[i]);
+    }
   }
+  
+  console.log(greenChar);
+  console.log(yellowChar);
+  console.log(grayChar);
 
   for (i = 0; i < 5; i++) {
     setTimeout(updateStyle, 500 * i, form[i], colors[i]);
   }
-
-  // findValidGuesses(ANSWER);
+  guess = guess.map((item) => item.toLowerCase());
+  findValidGuesses(guess, colors);
 };
 
 const updateStyle = (cell, color) => {
   cell.classList.add(`flip-${color}`);
+  const key = document.getElementById(cell.value);
+  key.classList.remove("bg-[#818384]");
+  if (color == "green") {
+    key.classList.add("bg-[#5a9a54]");
+    key.classList.remove("bg-[#b59f3b]");
+  } else if (color == "yellow" && !key.classList.contains("bg-[#5a9a54]")) {
+    key.classList.add("bg-[#b59f3b]");
+  } else if (
+    !key.classList.contains("bg-[#5a9a54]") &&
+    !key.classList.contains("bg-[#b59f3b]")
+  ) {
+    key.classList.add("bg-[#3a3a3c]");
+  }
+};
+
+const findValidGuesses = (guess, colors) => {
+  if (!wordList || wordList.length == 0) return null;
+  
+  // for (i = 0; i < 5; i++) {
+  //   if (greenChar[i] != "") {
+  //     let k = 0;
+  //     while (k < wordList.length) {
+  //       if (wordList[k][i] != greenChar[i]) {
+  //         wordList.splice(i, 1);
+  //       } else k++;
+  //     }
+  //   }
+  // }
+
+  // while (i < wordList.length) {
+  //   let word = wordList[i];
+  //   for (j = 0; j < 5; j++) {
+  //     if (colors[j] === "gray") {
+  //       let k = 0;
+  //       if (!removedLetters.includes(guess[j])) {
+  //         while (k < wordList.length) {
+  //           if (wordList[k].includes(guess[j])) {
+  //             wordList.splice(k, 1);
+  //           } else {
+  //             k++;
+  //           }
+  //         }
+  //         removedLetters.push(guess[j]);
+  //       }
+  //     } else {
+  //       if (!existingLetters.includes(guess[j])) {
+  //         existingLetters.push(guess[j]);
+  //       }
+  //     }
+  //   }
+
+  //   // for (j = 0; j < 5; j++) {
+  //   //   if (colors[j] === "green") {
+  //   //     if (word[j] != guess[j]) {
+  //   //       wordList.splice(i, 1);
+  //   //       i--;
+  //   //       break;
+  //   //     }
+  //   //   }
+  //   // }
+
+  //   //   // if (colors[j] == "yellow") {
+  //   //   //   let oneMatch = false;
+  //   //   //   for (k = 0; k < 5; k++) {
+  //   //   //     if (word[k] == guess[j]) {
+  //   //   //       word[k] = 'x';
+  //   //   //       oneMatch = true;
+  //   //   //     }
+  //   //   //   }
+  //   //   // }
+  //   // }
+  //   i++;
+  // }
+  // wordList = newWordList;
+  // console.log(wordList);
+  const validGuessDisplay = document.getElementById("right");
+  validGuessDisplay.textContent = wordList.join(" ");
 };
